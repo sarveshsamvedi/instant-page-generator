b614a8f1e2d0fac741e1ec44306150dd1a3a5ae9
Vinay Patel <89964285+vinaypatel04@users.noreply.github.com>
2022-11-17 14:07:39 +0530

    Preview section (#3)
    * preview section WIP
    
    * Preview section v1
    
    * preview section changes

--- src/components/PreviewSection/HorizontalScroll.js
+++ src/components/PreviewSection/HorizontalScroll.js
@@ -6,7 +6,7 @@ const HorizontalScroll = ({ width = 360, height = 360, payload }) => {
         <div style={{ width: `${360}px`, height: `${height}px`, overflowX: 'scroll' }} className="flex">
             {
                 imgUrls.map((url, index) => {
-                    return <img src={url} alt='' style={{ width: `${width}px`, height: `${height}px` }} key={index} />
+                    return <img src={url} alt='' style={{ width: `${width}px`, height: `${height}px` }} className="mr-12" key={index} />
                 })
             }
         </div>

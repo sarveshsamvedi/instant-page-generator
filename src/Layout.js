1f18445164decbf746de1435688e39ffb19d7d06
ashu-tech-dev <ashutoshyadav@sharechat.co>
2022-11-17 13:05:40 +0530

    resolved conflicts

---
+++ src/Layout.js
@@ -0,0 +1,20 @@
+import { Sidebar, Menu, MenuItem, useProSidebar } from 'react-pro-sidebar';
+
+export default function Layout() {
+  const { collapseSidebar } = useProSidebar();
+
+  return (
+    <div style={{ display: 'flex', height: '100%' }}>
+      <Sidebar>
+        <Menu>
+          <MenuItem> Documentation</MenuItem>
+          <MenuItem> Calendar</MenuItem>
+          <MenuItem> E-commerce</MenuItem>
+        </Menu>
+      </Sidebar>
+      <main>
+        <button onClick={() => collapseSidebar()}>Collapse</button>
+      </main>
+    </div>
+  );
+}

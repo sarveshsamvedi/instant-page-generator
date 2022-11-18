6e6dfa7cf2567b5e25f9674936c1f16dcc22429c
ashu-tech-dev <ashutoshyadav@sharechat.co>
2022-11-18 09:12:28 +0530

    header margin reduced

--- src/components/shared/Header/index.jsx
+++ src/components/shared/Header/index.jsx
@@ -5,7 +5,7 @@ const { Header } = Layout;
 const HeaderComp = () => {
   return (
     <Header
-      style={{ background: "lightgrey", padding: 0, height: 50, fontSize: "2.2vw", padding: "2vw", marginBottom: "2vw" }}
+      style={{ background: "lightgrey", padding: 0, height: 50, fontSize: "2.2vw", padding: "2vw", marginBottom: "0.5vw" }}
       className="border-b border-gray-200 flex flex-row justify-between items-center pr-6 pl-8"
     >
       <div className="leftBlock">
